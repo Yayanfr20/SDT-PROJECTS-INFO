bd87159961e4e568e7a551bed4c5e75871d4542f
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-09-03 22:18:35 +0700

    update theme mode

--- script.js
+++ script.js
@@ -33,3 +33,61 @@ function openSidebar() {
 	sidebar.setAttribute("style", "animation: move2 1s;");
 	sidebar2.setAttribute("style", "");
 }
+
+let icon = document.getElementById("icon");
+let srcIcon = localStorage.getItem('icon');
+
+if(!srcIcon) {
+	srcIcon = "bi bi-brightness-high";
+}
+icon.setAttribute("class", srcIcon);
+
+let icon2 = document.getElementById("icon2");
+let srcIcon2 = localStorage.getItem('icon');
+
+if(!srcIcon2) {
+	srcIcon2 = "bi bi-brightness-high";
+}
+
+icon2.setAttribute("class", srcIcon2);
+
+let btnModeTheme = document.getElementById("btn-mode");
+let srcBtn = localStorage.getItem('btn');
+
+if(!srcBtn) {
+	srcBtn = "themeMode(false);";
+}
+
+btnModeTheme.setAttribute("onclick", srcBtn);
+
+let btnModeTheme2 = document.getElementById("btn-mode2");
+let srcBtn2 = localStorage.getItem('btn');
+
+if(!srcBtn2) {
+	srcBtn2 = "themeMode(false);";
+}
+
+btnModeTheme2.setAttribute("onclick", srcBtn2);
+
+let conBody = document.getElementById("con");
+let theme = localStorage.getItem('theme');
+if(!theme) {
+	theme = "white-mode";
+}
+
+conBody.setAttribute("class",theme);
+
+function themeMode(mode) {
+	if(mode == true) {
+		localStorage.setItem('icon', 'bi bi-brightness-high');
+		localStorage.setItem('theme', 'dark-mode');
+		localStorage.setItem('btn', "themeMode(false);");
+		document.location.href = 'index.html';
+
+	}else if (mode == false) {
+		localStorage.setItem('icon', 'bi bi-moon');
+		localStorage.setItem('theme', 'white-mode');
+		localStorage.setItem('btn', "themeMode(true);");
+		document.location.href = 'index.html';
+	}
+}
